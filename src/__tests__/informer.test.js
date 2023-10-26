0dfc3db589baf10637554a85971c1cd5f630fc52
AndrK189100 <andrka@gmail.com>
2023-10-26 19:03:13 +0300

    end

---
+++ src/__tests__/informer.test.js
@@ -0,0 +1,36 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import Informer from "../js/informer/informer";
+
+test('must show informer on click button', () => {
+
+    document.body.innerHTML = '<button class="button1" data-toggle="informer" data-header="Кнопка 1" data-body="Это кнопка 1 по клику, событие по умолчанию">CLICK1</button>';
+         
+    Informer.init();
+
+    const button = document.querySelector('.button1');
+    button.click();
+
+    const element = !document.querySelector('.informer');
+    
+     expect(element).toBe(false);
+
+})
+
+test('must hide informer on click button', () => {
+    
+    document.body.innerHTML = '<button class="button1" data-toggle="informer" data-header="Кнопка 1" data-body="Это кнопка 1 по клику, событие по умолчанию">CLICK1</button>';
+         
+    Informer.init();
+
+    const button = document.querySelector('.button1');
+    button.click();
+    button.click();
+
+    const element = !document.querySelector('.informer');
+
+    expect(element).toBe(true);
+
+})
